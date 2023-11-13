176789f911a695d04a00937c9df0a86fdace82a6
Nathan Clark <nclark@icstars.org>
2023-11-13 14:21:43 -0600

    setup gh-pages

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <p>hello world!</p>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/careers">Careers</Link>
