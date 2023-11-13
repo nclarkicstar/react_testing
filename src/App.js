a0a1c30cc26e17dce7f6fa4f58be79a874970904
Nathan Clark <nclark@icstars.org>
2023-11-13 14:07:18 -0600

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import About from "./routes/About";
 import Careers from "./routes/Careers";
 import Home from "./routes/Home";
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
